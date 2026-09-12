00662d02f9ed1f8462fdcaa3a26763b05b4198f2
liuran203-sudo <liuran203@gmail.com>
2026-09-13 01:10:40 +0800

    Site updated: 2026-09-13 01:10:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/08/30/20260902/","2026/09/12/从零搭建Hexo博客并部署到GitHub/"];function toRandomPost(){
+var posts=["posts/20dcad40/","posts/6b218ca2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
